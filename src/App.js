87b3c2f162507972149c7456fbbd14195547d2ac
Brett <bnicholas10@gmail.com>
2022-10-25 21:35:41 -0400

    initial state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+
+  const checkToken = () => {
+    if (token === "" && localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+      console.log(`fetched token from local`);
+    }
+  };
+  checkToken();
+
+  useEffect(() => {}, [token]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>TEST TEXT</p>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = (props) => {
+  return <div id="navbar">Navbar here</div>;
+};
+
+export default Navbar;
